9ccaf168d031d47a1b89bee1acf53cb951c22e80
Muttu <kagalmutturaj@gmail.com>
2025-08-14 16:18:53 +0530

    After UC2

---
+++ src/oops/day6/input/ConsoleInputProvider.java
@@ -0,0 +1,30 @@
+package oops.day6.input;
+
+import oops.day6.contact.Contact;
+
+import java.util.Scanner;
+
+public class ConsoleInputProvider implements GetInputProvider {
+    Scanner scan=new Scanner(System.in);
+    @Override
+    public Contact getInputDetails() {
+        Contact person=new Contact();
+        System.out.println("Enter person First Name: ");
+        person.setFirstName(scan.next());
+        System.out.println("Enter person Last Name: ");
+        person.setLastName(scan.next());
+        System.out.println("Enter person city : ");
+        person.setCity(scan.next());
+        System.out.println("Enter person state Name: ");
+        person.setState(scan.next());
+        System.out.println("Enter person Address Name: ");
+        person.setAddress(scan.next());
+        System.out.println("Enter person Zip code: ");
+        person.setZipCode(scan.nextInt());
+        System.out.println("Enter person Phone number : ");
+        person.setPhoneNumber(scan.nextLong());
+        System.out.println("Enter person email : ");
+        person.setEmail(scan.next());
+        return person;
+    }
+}
---
+++ src/oops/day6/input/GetInputProvider.java
@@ -0,0 +1,7 @@
+package oops.day6.input;
+
+import oops.day6.contact.Contact;
+
+public interface GetInputProvider {
+    Contact getInputDetails();
+}
