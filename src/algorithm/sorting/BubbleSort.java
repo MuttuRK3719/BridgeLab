838ae305998d35b699f00faaca5787a355de910c
Muttu <kagalmutturaj@gmail.com>
2025-08-18 12:51:05 +0530

    Implementing Bubble sort

---
+++ src/algorithm/sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+package algorithm.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 5, 9, 7, 8};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+}
